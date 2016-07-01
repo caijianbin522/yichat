bf7e7d658283005257bb1c0e9fa6da0bbea50f3f
路西法 <jianbin522@qq.com>
2016-07-01 14:19:03 +0800

    创建工程

---
+++ easyChat/easyChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  easyChat
+//
+//  Created by mac on 16/7/1.
+//  Copyright © 2016年 艾尼维亚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
